07cb2f07cc870e3165d2baa21ea89b7dcfb566dc
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-10 12:23:23 +0200

    .

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+// @ts-nocheck
+  import Kosar from './lib/Kosar.svelte'
+</script>
+
+<main>
+  <div class="card">
+    <Kosar termekek={[
+      {nev: 'sajt ömlesztett 100g', ar: 2000, db: 1},
+      {nev: 'tejföl 200g tesco', ar: 1800, db: 1},
+    ]}/>
+
+  </div>
+
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Kosar.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let termekek
+</script>
+<h1>
+  Kosár
+  <hr>
+  {#each termekek as item}
+    {item.nev} {item.db} {item.db}
+    <br>
+  {/each}
+</h1>
