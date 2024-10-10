3824245afd30978b3974dc79924dca687b0b698f
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-10 16:34:26 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,47 @@
 <script>
-// @ts-nocheck
-  import Kosar from './lib/Kosar.svelte'
+  // @ts-nocheck
+  import Kosar from "./lib/Kosar.svelte";
   var termekek = [
-      {nev: 'sajt ömlesztett 100g', ar: 2000, db: 1},
-      {nev: 'tejföl 200g tesco', ar: 1800, db: 1},
-    ]
-  var j = 0
+    { nev: "sajt ömlesztett 100g", ar: 2000, db: 1 },
+    { nev: "tejföl 200g tesco", ar: 1800, db: 1 },
+  ];
+  var j = 0;
 </script>
 
 <main>
-  {#each termekek as termek}
-    {termek.nev} {termek.ar}Ft - {termek.db} db
-  {/each}
-  <div class="card">
-    <Kosar bind:termekek={termekek}/>
+  <div class="cc">
+    {#each termekek as termek}
+      <div class="elem">{termek.nev} {termek.ar}Ft - {termek.db} db</div>
+    {/each}
   </div>
-  <input type="range" min=0 max=6.28 step=0.01 bind:value={j}>
-
-  <div class="c" style="top:{300 + 260*Math.sin(j)}px; left:{620+550*Math.cos(j)}px;"></div>
+  <div class="card">
+    <Kosar bind:termekek />
+  </div>
+  <input type="range" min="0" max={12*Math.PI} step="0.01" bind:value={j} />
+  <div
+    class="c"
+    style="top:{300 + 260 * Math.sin(j)}px; left:{620 + 550 * Math.cos(j)}px;"
+  ></div>
 </main>
 
 <style>
-div.c {
-  position: fixed;
-  background-color: thistle;
-  width: 30px;
-  height: 30px;
-  border-radius: 15px;
-}
+  div.cc {
+    display: flex;
+  }
+  div.elem {
+    background-color: black;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 10px;
+  }
+  div.c {
+    position: fixed;
+    background-color: thistle;
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+  }
+  input {
+    width: 600px;
+  }
 </style>
--- src/lib/Kosar.svelte
+++ src/lib/Kosar.svelte
@@ -1,20 +1,27 @@
 <script>
   export let termekek
 </script>
-<h1>
-  Kosár
-  <hr>
-  {#each termekek as item}
-    <b>{item.nev}</b> {item.ar}Ft - <input type="number" bind:value={item.db} max=99 min=1> db
-    <br>
-  {/each}
-</h1>
+<h1>Kosár</h1>
+<hr>
+<div>
+{#each termekek as item}
+  <b>{item.nev}</b> <i>{item.ar}Ft</i> - <input type="number" bind:value={item.db} max=99 min=1> db
+  <br>
+{/each}
+</div>
+
 <style>
+  i {
+    color: rgb(112, 198, 112);
+  }
   b {
-    color: blue;
+    color: rgb(156, 156, 217);
+  }
+  div {
+    font-size: 30px;
   }
   input {
-    font-size: 40px;
+    font-size: 30px;
     width: 90px;
   }
 </style>
