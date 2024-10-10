d214dfa5aa807fb17008ec2ae076a45242c42fa0
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-10 13:05:44 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,31 @@
 <script>
 // @ts-nocheck
   import Kosar from './lib/Kosar.svelte'
+  var termekek = [
+      {nev: 'sajt ömlesztett 100g', ar: 2000, db: 1},
+      {nev: 'tejföl 200g tesco', ar: 1800, db: 1},
+    ]
+  var j = 0
 </script>
 
 <main>
+  {#each termekek as termek}
+    {termek.nev} {termek.ar}Ft - {termek.db} db
+  {/each}
   <div class="card">
-    <Kosar termekek={[
-      {nev: 'sajt ömlesztett 100g', ar: 2000, db: 1},
-      {nev: 'tejföl 200g tesco', ar: 1800, db: 1},
-    ]}/>
-
+    <Kosar bind:termekek={termekek}/>
   </div>
+  <input type="range" min=0 max=6.28 step=0.01 bind:value={j}>
 
+  <div class="c" style="top:{300 + 260*Math.sin(j)}px; left:{620+550*Math.cos(j)}px;"></div>
 </main>
 
 <style>
-
+div.c {
+  position: fixed;
+  background-color: thistle;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+}
 </style>
--- src/lib/Kosar.svelte
+++ src/lib/Kosar.svelte
@@ -5,12 +5,16 @@
   Kosár
   <hr>
   {#each termekek as item}
-    <b>{item.nev}</b> {item.ar}Ft - {item.db} db
+    <b>{item.nev}</b> {item.ar}Ft - <input type="number" bind:value={item.db} max=99 min=1> db
     <br>
   {/each}
 </h1>
 <style>
   b {
     color: blue;
+  }
+  input {
+    font-size: 40px;
+    width: 90px;
   }
 </style>
